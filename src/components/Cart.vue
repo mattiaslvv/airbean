707531e6ffa0eef2632dd4d9632e1e6b8a1a9227
Lijuancheng <pushuhexi11@hotmail.com>
2020-03-18 21:38:44 +0100

    Added scripts to components

---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Din beställning</h1>
+      <CartItem v-for="item in items" :key="item.id" v-bind:item="item" />
+      <button>Take my money!</button>
+  </div>
+</template>
+
+<script>
+import CartItem from "./CartItem"
+export default {
+    components:{
+        CartItem,
+    },
+    props:{
+        items:Array
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  {{item}}
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        item:Object
+    }
+
+}
+</script>
+
+<style>
+
+</style>
