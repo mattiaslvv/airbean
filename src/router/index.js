69587796ad38d68b0e42d189b025a73402d99fd1
frotuck <54941820+frotuck@users.noreply.github.com>
2020-03-18 15:16:54 +0100

    added all components and views needed for project i think hehe

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Menu from '../views/Menu.vue'
+import OrderStatus from '../views/OrderStatus.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/',
+    name: 'Menu',
+    component: Menu
+  },
+  {
+    path: '/',
+    name: 'OrderStatus',
+    component: OrderStatus
+  },
 ]
 
 const router = new VueRouter({
