be8368827ecf06147b4c80ea22abbc43ed0dc619
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-03-18 17:22:11 +0100

    fixed API call to show menu

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api/beans"
+})
+
+export async function getItems() {
+  const response = await api.get()
+  const data = response.data.menu
+  return data
+}
+
+export async function postItems() {
+  const response = await api.post()
+  const data = response.data
+  return data
+}
+
+// export default {
+//   postItems,
+//   getItems
+// }
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as API from '@/api/index.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cart: [],
+    cartItem: {},
+    totalprice: 0,
+    menuItems: [],
+    orderInfo: {}
   },
   mutations: {
+    getMenu(state, data) {
+      state.menuItems = data
+    },
+    postOrder(state, data) {
+      state.orderInfo = data
+    }
   },
   actions: {
+    async fetchMenuItems(context) {
+      const data = await API.getItems()
+      context.commit('getMenu', data)
+      return true
+    },
+    async postOrderItems(context) {
+      const data = await API.postItems()
+      context.commit('postOrder', data)
+      return true
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
