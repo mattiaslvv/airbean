a6e584db7016e748179f53b8d7c00f91873314fd
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-03-18 15:16:43 +0100

    conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
     <img alt="Vue logo" src="../assets/logo.png" />
-=======
-    <img alt="Vue logo" src="../assets/logo.png">
->>>>>>> 9652ee6996eaa1cba2153f3a4b27943b29442fc7
   </div>
 </template>
 
@@ -12,12 +8,8 @@
 // @ is an alias to /src
 
 export default {
-<<<<<<< HEAD
-  name: 'Home'
-=======
   name: 'Home',
   components: {
   }
->>>>>>> 9652ee6996eaa1cba2153f3a4b27943b29442fc7
 }
 </script>
