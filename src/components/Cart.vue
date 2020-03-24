23b5c44c2e037f192247051c878fd593688185e8
Lijuancheng <pushuhexi11@hotmail.com>
2020-03-24 19:49:18 +0100

    fixade styling about cart and loader

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,66 +1,77 @@
 <template>
-  <div class="cartBground" v-if="showCart">
-    <h1 class="cart">Din beställning</h1>
-    <img v-if="loading" src="../assets/graphics/loader.png" />
-    <div v-else>
-      <CartItem
-        v-for="item in cartItems"
-        :key="item.id"
-        v-bind:item="item"
-        :countItem="count(item)"
-      />
-      <h2 class="total">Total</h2>
-      <span>{{totalPrice}}</span>
-      <span>inkl moms + drönarleverans</span>
+  <div class="overBground" v-if="showCart">
+    <div class="cartBground">
+      <h1 class="cart">Din beställning</h1>
+      <div v-if="loading" id="loader">
+        <img src="../assets/graphics/loader.png" />
+      </div>
+      <div class="list" v-else>
+        <CartItem
+          v-for="item in cartItems"
+          :key="item.id"
+          v-bind:item="item"
+          :countItem="count(item)"
+        />
+      </div>
+      <div class="payment">
+        <div class="price">
+          <h2 class="total">Total</h2>
+          <h2>{{totalPrice}} kr</h2>
+        </div>
+        <span>inkl moms + drönarleverans</span>
+        <button class="pay" @click="postItems">Take my money!</button>
+      </div>
     </div>
-    <button @click="postItems">Take my money!</button>
   </div>
 </template>
 
 <script>
-import CartItem from "./CartItem"
-import { mapGetters } from 'vuex'
+import CartItem from "./CartItem";
+import { mapGetters } from "vuex";
 export default {
-    components:{
-        CartItem,
+  components: {
+    CartItem
+  },
+  data() {
+    return {
+      loading: false
+    };
+  },
+  computed: {
+    ...mapGetters(["totalPrice"]),
+    items() {
+      return this.$store.state.cart;
     },
-     data(){ return {    
-    loading: false,
-  }},
-    computed:{
-        ...mapGetters(['totalPrice']),
-        items() {
-            return this.$store.state.cart
-        },
-        cartItems() {
-            // bara använda varje id en gång
-            let idMap = [...new Set(this.items.map(item => item.id))]
-            // hitta item för alla id
-            return idMap.map(id => this.items.find(item => item.id == id))
-        },
-        showCart() {
-            return this.$store.state.showCart
-        }
+    cartItems() {
+      // bara använda varje id en gång
+      let idMap = [...new Set(this.items.map(item => item.id))];
+      // hitta item för alla id
+      return idMap.map(id => this.items.find(item => item.id == id));
     },
-    methods:{
-        count(item){
-           return this.items.filter(item2 =>item2.id === item.id).length
-        },
-        //TODO: add loading spinner while awaiting response from API call
-        //TODO: lös duplicated keys error om man går till orderstatus, sen tillbaka till menu och beställer mer
-        async postItems(){
-            this.loading = true
-            await this.$store.dispatch('postOrderItems')
-            this.$router.push('/orderstatus')
-            this.loading = false
-        }
+    showCart() {
+      return this.$store.state.showCart;
     }
-
-}
+  },
+  methods: {
+    count(item) {
+      return this.items.filter(item2 => item2.id === item.id).length;
+    },
+    async postItems() {
+      this.loading = true;
+      await this.$store.dispatch("postOrderItems");
+      this.$router.push("/orderstatus");
+      this.loading = false;
+    }
+  }
+};
 </script>
 
 <style>
 @import "./../assets/scss/main.scss";
+h1 {
+  text-align: center;
+  font-size: 2rem;
+}
 .cart {
   padding: 1rem;
   top: 4rem;
@@ -68,9 +79,59 @@ export default {
   margin: 1rem;
   background-color: white;
   border-radius: 0.3rem;
+  text-align: center;
 }
 .cartBground {
+  position: relative;
+  top: 100px;
   background-color: white;
+  height: 950px;
   color: black;
+
+  width: 450px;
+}
+
+#loader {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+#loader img {
+  width: 30%;
+  background-color: pink;
+}
+.overBground {
+  background-color: rgba(34, 34, 34, 0.7);
+  width: 100%;
+  height: 1070px;
+  display: flex;
+  justify-content: center;
+}
+.list {
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 40px 20px;
+}
+.price {
+  display: flex;
+  justify-content: space-between;
+}
+
+.payment {
+  margin: 0 20px 20px 20px;
+}
+.pay {
+  width: 300px;
+  background-color: rgb(14, 3, 3);
+  color: white;
+  border-radius: 30px;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+  cursor: pointer;
+  text-align: center;
+  margin: 30px 0 0 55px;
+  outline: none;
 }
 </style>
--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,33 +1,51 @@
 <template>
-  <div>
-    <h2>{{item.title}}</h2>
-    <h6>{{item.price}}</h6>
-    <img src="@/assets/graphics/arrow-up.svg" alt="Arrow up" @click="addToCart(item.id)" />
-    <h6>{{countItem}}</h6>
-    <img src="@/assets/graphics/arrow-down.svg" alt="Arrow down" @click="removeFromCart(item.id)" />
+  <div class="cartListItem">
+    <div>
+      <h2>{{item.title}}</h2>
+      <h6 class="price">{{item.price}}kr</h6>
+    </div>
+    <div class="listIcon">
+      <img src="@/assets/graphics/arrow-up.svg" alt="Arrow up" @click="addToCart(item.id)" />
+      <h6 class="count">{{countItem}}</h6>
+      <img src="@/assets/graphics/arrow-down.svg" alt="Arrow down" @click="removeFromCart(item.id)" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props:{
-        item:Object,
-        countItem:Number
+  props: {
+    item: Object,
+    countItem: Number
+  },
+  methods: {
+    addToCart(id) {
+      this.$store.dispatch("addThisToCart", id);
     },
-    methods: {
-      addToCart(id) {
-        this.$store.dispatch('addThisToCart', id)
-      },
-      removeFromCart(id) {
-        this.$store.dispatch('removeThisFromCart', id)
-      }
+    removeFromCart(id) {
+      this.$store.dispatch("removeThisFromCart", id);
     }
-
-}
+  }
+};
 </script>
 
 <style>
+.cartListItem {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
 #remove {
   transform: rotate(-180deg);
+}
+.listIcon {
+  margin: 10px 20px 10px 0;
+}
+.count {
+  margin: 0;
+  text-align: center;
+}
+.price {
+  margin: 0;
 }
 </style>
